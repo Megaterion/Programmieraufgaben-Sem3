c54ac39240011af2fba86c331ec4740edb0e6c4d
Megaterion <megaterion03@gmail.com>
2022-09-28 14:40:37 +0200

    code schick machen

--- Programmieraufgaben Sem3.cpp
+++ Programmieraufgaben Sem3.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-
 #include "aufgabe17.h"
-
 
 int main()
 {
@@ -9,30 +7,23 @@ int main()
     std::cout << "Welche Aufgabe willst du ausfueren?\n";
     std::cin >> input;
 
-
     switch (input)
     {
-    case 1:
-        std::cout << "error\n";
-        break;
     case 17:
         std::cout << "Aufgabe_17 wird ausgefuehrt\n";
         aufgabe();
         break;
     case 18:
-        std::cout << "error\n";
+        std::cout << "unknown\n";
         break;
     case 19:
-        std::cout << "error?\n";
+        std::cout << "unknown\n";
         break;
     default:
-        std::cout << "unknown!" << std::endl;
+        std::cout << "Fehler!" << std::endl;
         break;
     }
     std::cout << "**************************************************" << std::endl;
 
     return 0;
 }
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
